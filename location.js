af43ea8135898ea91550ca91beb9a40b0a5ba3a2
avinashvishu <loginavi@gmail.com>
2022-07-03 01:32:16 +0530

    json

--- location.js
+++ location.js
@@ -15,17 +15,17 @@
 //     }
 // }
 
-function showPosition(data) {
-    console.log(data)
-    let latitude = data.coords.latitude;
-    let longitude = data.coords.longitude;
-    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
-    fetch(url, {
-            method: 'GET'
-        })
-        .then((res) => console.log(res.json()))
-        .then((data) => {
-            console.log(data)
-            let cityName = data.city.cityName
-            x.innerText = ` ${cityName}`;
-        })
+// function showPosition(data) {
+//     console.log(data)
+//     let latitude = data.coords.latitude;
+//     let longitude = data.coords.longitude;
+//     const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+//     fetch(url, {
+//             method: 'GET'
+//         })
+//         .then((res) => console.log(res.json()))
+//         .then((data) => {
+//             console.log(data)
+//             let cityName = data.city.cityName
+//             x.innerText = ` ${cityName}`;
+//         })
